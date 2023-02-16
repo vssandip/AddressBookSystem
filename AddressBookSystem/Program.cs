98c9b40976f166fcf1f557811461d84e8c8171bd
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-16 13:11:33 +0530

    [ SANDIP ] Adding  'Ability to create a Contacts in Address Book'

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -13,5 +13,27 @@
             addressBook.print();
             Console.ReadLine();
         }
+        // take Input And Add To Contacts
+        public static void takeInputAndAddToContacts(AddressBook addressBook)
+        {
+            Console.WriteLine("Enter FirstName");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email id");
+            string email = Console.ReadLine();
+            addressBook.addContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            Console.ReadLine();
+        }
     }
 }
