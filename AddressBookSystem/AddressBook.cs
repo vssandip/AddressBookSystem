2acd0e42044e45e9ebff78a3c64499d7363cd228
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-16 13:42:11 +0530

    [ SANDIP ] Adding  'Ability to Delete Existing Contact'

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -186,5 +186,24 @@ namespace AddressBookSystem
             Console.WriteLine("PhoneNumber: " + contact.phoneNumber);
             Console.WriteLine("Email id: " + contact.email);
         }
+
+        public void delete(string firstName, string lastName)
+        {
+            Contact contactToBeDeleted = null;
+            foreach (Contact contact in this.contactList)
+            {
+                if (contact.fistName == firstName && contact.lastName == lastName)
+                {
+                    contactToBeDeleted = contact;
+                    this.contactList.Remove(contactToBeDeleted);
+                    break;
+                }
+            }
+            if (contactToBeDeleted == null)
+                Console.WriteLine("No such contact exists");
+            else
+                Console.WriteLine("Deletion Done.");
+        }
+
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -14,6 +14,7 @@
             //    // called Print method 
             //    addressBook.print();
             //    Console.ReadLine();
+
             //creating object of addressbook
             AddressBook addressBook = new AddressBook();
             //getting Op in Constructor with paratmers
@@ -21,12 +22,28 @@
             takeInputAndAddToContacts(addressBook);
             // called Print method 
             addressBook.print();
-            Console.ReadLine();
-            Console.WriteLine("Enter FirstName of Contact to be edited");
-            string firstNameOfContactToBeEdited = Console.ReadLine();
-            Console.WriteLine("Enter LastName of Contact to be edited");
-            string lastNameOfContactToBeEdited = Console.ReadLine();
-            addressBook.edit(firstNameOfContactToBeEdited, lastNameOfContactToBeEdited);
+            Console.WriteLine("What you want to perform ? Press 1 for Edit the details ,\n Press 2 for Delete  details : ");
+            int Selectchoice = Convert.ToInt32(Console.ReadLine());
+            switch (Selectchoice)
+            {
+
+                case 1:
+                    Console.WriteLine("Enter FirstName of Contact to be edited");
+                    string firstNameOfContactToBeEdited = Console.ReadLine();
+                    Console.WriteLine("Enter LastName of Contact to be edited");
+                    string lastNameOfContactToBeEdited = Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter FirstName of Contact to be deleted");
+                    string firstNameOfContactToBeDeleted = Console.ReadLine();
+                    Console.WriteLine("Enter LastName of Contact to be deleted");
+                    string lastNameOfContactToBeDeleted = Console.ReadLine();
+                    addressBook.delete(firstNameOfContactToBeDeleted, lastNameOfContactToBeDeleted);
+                    break;
+                default:
+                    Console.WriteLine("Please enter the valid number : ");
+                    break;
+            }
             Console.ReadLine();
 
         }
